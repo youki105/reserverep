d7d8fa73fbd59447b97eb66ace2e8812ee50ebd3
youki105 <youki105@yahoo.com>
2026-02-26 22:48:15 +0300

    Fix port binding

--- index.js
+++ index.js
@@ -252,7 +252,8 @@ function escapeXml(str) {
     .replace(/'/g, "&apos;");
 }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
